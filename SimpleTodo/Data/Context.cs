93dfeb19a37491481c8deb0fd31d5225a8275612
Mario Felippe <mfchaves@live.com>
2022-05-12 17:02:27 -0300

    feat: Adicionado Migrate. no construtor.

--- SimpleTodo/Data/Context.cs
+++ SimpleTodo/Data/Context.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTodo.Models;
+using System.Configuration;
 
 namespace SimpleTodo.Data
 {
-    public class Context
+    public class Context : DbContext
     {
+        public DbSet<Todo> Todos { get; set; }
+
+        public Context(): base()
+        {
+            try
+            {
+                Database.Migrate();
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Erro ao iniciar a base! {ex.Message}");
+            }
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            try
+            {
+                optionsBuilder.UseSqlite(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+            }catch (Exception ex)
+            {
+                throw new ConfigurationErrorsException("Erro ao iniciar o banco", ex);
+            }
+           
+        }
     }
 }
