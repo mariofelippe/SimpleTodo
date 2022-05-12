73963df320fe26bda004cfcb966be2f55eeee0b4
Mario Felippe <mfchaves@live.com>
2022-05-12 17:10:36 -0300

    refactor: Ajuste na exibição do Todo.

--- SimpleTodo/Models/Todo.cs
+++ SimpleTodo/Models/Todo.cs
@@ -2,10 +2,23 @@
 {
     public class Todo
     {
+        public Todo(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            IsDone = false;
+            CreatedDate = DateTime.Now;
+        }
         public int Id { get; private set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public bool IsDone { get; set; }    
         public DateTime CreatedDate { get; set; }
+
+        public override string ToString()
+        {
+            string status = IsDone ? "Concluída" : "Pendente";
+            return $"| {Id} | {Title} | {Description} | { status } | {CreatedDate} |";
+        }
     }
 }
