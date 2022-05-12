c32ba8765bcf8df2e43e2ef0a1127e496b978c1e
Mario Felippe <mfchaves@live.com>
2022-05-12 14:05:25 -0300

    feat: Adição CreatedDate e Isdone.

--- SimpleTodo/Models/Todo.cs
+++ SimpleTodo/Models/Todo.cs
@@ -5,5 +5,7 @@
         public int Id { get; private set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public bool IsDone { get; set; }    
+        public DateTime CreatedDate { get; set; }
     }
 }
